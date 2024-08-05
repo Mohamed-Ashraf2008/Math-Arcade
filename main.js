d333af352c5bf89db8b3dcfe86b91818b10251f6
Mohamed-Ashraf2008 <151741823+Mohamed-Ashraf2008@users.noreply.github.com>
2024-08-06 02:36:08 +0300

    new project

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const page0 = document.querySelector(".page0")
+const page1 = document.querySelector(".page1")
+
 function open(){
     page0.classList.add("page0-op")
 }
 window.addEventListener("load", open)
 function clPage0(){
     page0.classList.remove("page0-op")
+    page1.classList.add("page1-op")
 }
