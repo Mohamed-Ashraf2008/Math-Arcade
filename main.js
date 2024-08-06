b0d151b44d41b15a2cd2b8bc5abd1762887033d8
Mohamed-Ashraf2008 <151741823+Mohamed-Ashraf2008@users.noreply.github.com>
2024-08-06 17:31:44 +0300

    made the upper half of the secound page

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const page0 = document.querySelector(".page0")
 const page1 = document.querySelector(".page1")
-
+const name = document.querySelector("#name")
 function open(){
     page0.classList.add("page0-op")
 }
 window.addEventListener("load", open)
 function clPage0(){
+    let playerName = name.value
+    console.log(playerName)
     page0.classList.remove("page0-op")
     page1.classList.add("page1-op")
 }
