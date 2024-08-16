e673999597a3193b41f047202d2952864b0add3c
ma3972115 <ma3972115@gmail.com>
2024-08-16 06:57:37 +0000

    almost finished

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const page0 = document.querySelector(".page0")
 const page1 = document.querySelector(".page1")
+const page2 = document.querySelector(".page2")
 const name = document.querySelector("#name")
+let sing 
+let mode
 function open(){
     page0.classList.add("page0-op")
 }
@@ -10,4 +13,31 @@ function clPage0(){
     console.log(playerName)
     page0.classList.remove("page0-op")
     page1.classList.add("page1-op")
+}
+function clpage1(){
+    page1.classList.remove("page1-op")
+    page2.classList.add("page2-op")
+}
+function clpage2(){
+    page2.classList.remove("page2-op")
+    page3.classList.add("page3-op")
+}
+function add(){
+    sing = "+"
+    mode = "add"
+}
+function sub(){
+    sing = "-"
+    mode = "sub"
+}
+function mul(){
+    sing = "X"
+    mode = "mul"
+}
+function div(){
+    sing = "/"
+    mode = "div"
+}
+function mix(){
+    mode = "mix"
 }
